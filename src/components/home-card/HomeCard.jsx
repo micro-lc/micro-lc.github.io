7e0db4c576adabc32f73589aa3d957e48e273de6
iLTeoooD <matteo.dazzi@mia-platform.eu>
2021-12-22 15:26:42 +0100

    Make all cards clickable

--- src/components/home-card/HomeCard.jsx
+++ src/components/home-card/HomeCard.jsx
@@ -5,24 +5,23 @@ import PropTypes from 'prop-types'
 import useBaseUrl from '@docusaurus/useBaseUrl'
 import DocusaurusLink from '@docusaurus/Link'
 import {Link as ReactLink} from 'react-router-dom'
+import clsx from 'clsx'
 
 import styles from '../home-card/HomeCard.module.css'
 
 function HomeCard ({Svg, title, description, navigateTo, isExternalMicroFrontEndLink = false}) {
   return (
-    <Link className={styles.home_card_explore} to={useBaseUrl(navigateTo)} useReactLink={isExternalMicroFrontEndLink}>
-      <div className={styles.home_card_container}>
-          { Svg && <Svg alt={title} className={styles.featureSvg}/> }
-          { !Svg && <div className={styles.home_card_svg_placeholder}/> }
-          <div className={styles.home_card_text_content}>
-              <p className={styles.home_card_title}>{title}</p>
-              <p className={styles.home_card_description}>{description}</p>
-          </div>
-          <div>
-            {'Explore'}
-            <FontAwesomeIcon className={styles.home_card_explore_icon} icon={faLongArrowAltRight}/>
-          </div>
-      </div>
+    <Link className={clsx(styles.home_card_explore, styles.home_card_container)} to={useBaseUrl(navigateTo)} useReactLink={isExternalMicroFrontEndLink}>
+        { Svg && <Svg alt={title} className={styles.featureSvg}/> }
+        { !Svg && <div className={styles.home_card_svg_placeholder}/> }
+        <div className={styles.home_card_text_content}>
+            <p className={styles.home_card_title}>{title}</p>
+            <p className={styles.home_card_description}>{description}</p>
+        </div>
+        <div>
+          {'Explore'}
+          <FontAwesomeIcon className={styles.home_card_explore_icon} icon={faLongArrowAltRight}/>
+        </div>
    </Link>
   )
 }
