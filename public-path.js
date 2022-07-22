063e358cb6371afebaf8d7f9d480d948e9b0865a
Matteo Pietro Dazzi <matteo.dazzi@mia-platform.eu>
2022-07-22 19:55:47 +0200

    fixed anchor

--- public-path.js
+++ public-path.js
@@ -4,10 +4,22 @@ if (typeof window !== 'undefined' && window.__POWERED_BY_QIANKUN__) {
   __webpack_public_path__ = new URL(baseUrl, window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__).href
 }
 
+const onAClickListener = (e) => {
+  if (e.srcElement.tagName === 'A') {
+    const hrefValue = e.srcElement.getAttribute('href')
+    if(hrefValue.startsWith('#')) {
+      window.location.hash = hrefValue
+      e.preventDefault()
+    }
+  }
+}
+
 const QIANKUN_HANDLERS = {
   mount: async (props) => {
+    props.container?.addEventListener('click', onAClickListener);
   },
   unmount: async (props) => {
+    props.container?.removeEventListener('click', onAClickListener);
     return props.singleSpa.unloadApplication(props.name)
   },
   bootstrap: async (props) => {
