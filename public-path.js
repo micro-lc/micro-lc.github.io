671629a95a7cc0498f7fc177c70ad46256c6f357
Matteo Pietro Dazzi <matteo.dazzi@mia-platform.eu>
2022-07-31 23:15:30 +0200

    fix: hash on load

--- public-path.js
+++ public-path.js
@@ -4,22 +4,36 @@ if (typeof window !== 'undefined' && window.__POWERED_BY_QIANKUN__) {
   __webpack_public_path__ = new URL(baseUrl, window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__).href
 }
 
-const onAClickListener = (e) => {
-  if (e.srcElement.tagName === 'A') {
-    const hrefValue = e.srcElement.getAttribute('href')
+const onAClickListener = (event) => {
+  if (event.srcElement.tagName === 'A') {
+    const hrefValue = event.srcElement.getAttribute('href')
     if (hrefValue.startsWith('#')) {
       window.location.hash = hrefValue
-      e.preventDefault()
+      event.preventDefault()
     }
+  }
+}
+
+const domNodeInsertedListener = (event) => {
+  const isNavbarInserted = event.relatedNode.className === 'navbar navbar--fixed-top'
+  if(isNavbarInserted) {
+    const initialHash = window.location.hash
+    window.location.hash = ''
+    window.location.hash = initialHash
   }
 }
 
 const QIANKUN_HANDLERS = {
   mount: async (props) => {
-    props.container?.addEventListener('click', onAClickListener);
+    if(window.location.hash) {
+      props.container?.addEventListener('DOMNodeInserted', domNodeInsertedListener)
+    }
+    props.container?.addEventListener('click', onAClickListener)
   },
   unmount: async (props) => {
-    props.container?.removeEventListener('click', onAClickListener);
+    props.container?.removeEventListener('click', onAClickListener)
+    props.container?.removeEventListener('DOMNodeInserted', domNodeInsertedListener)
+
     return props.singleSpa.unloadApplication(props.name)
   },
   bootstrap: async (props) => {
