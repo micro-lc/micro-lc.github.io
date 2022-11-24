28547ad190ca2ddb3aa90c9ab1f12ed19d324e52
epessina <epessina@users.noreply.github.com>
2022-11-24 16:53:28 +0000

    deploy: dad03ae4d3c4f1b267e05eb0531d027046ab2658

---
+++ assets/js/14e72bca.e9901cf3.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocumentation=self.webpackChunkdocumentation||[]).push([[59],{9215:e=>{e.exports=JSON.parse('{"permalink":"/documentation/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
---
+++ assets/js/814f3328.cb58b3e1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocumentation=self.webpackChunkdocumentation||[]).push([[2535],{5641:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome micro-lc v2","permalink":"/documentation/blog/welcome-micro-lc-v2"}]}')}}]);
