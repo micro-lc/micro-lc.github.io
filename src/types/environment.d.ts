2641f74aedf98dba982df232f5843ffb37c27e8c
Umberto Toniolo <amountainram@gmail.com>
2023-02-09 22:16:06 +0100

    added .env file support to keep track of libs versions

--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -1,6 +1,13 @@
 export {}
 
 declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      MICRO_LC_LAYOUT_VERSION: string
+      MICRO_LC_ORCHESTRATOR_VERSION: string
+    }
+  }
+
   interface Window {
     MonacoEnvironment: {
       getWorker(_: unknown, label: string): Worker | Promise<Worker>
