dc44423406b49742470ef4c78dede45aa506d964
Umberto Toniolo <amountainram@gmail.com>
2023-02-09 15:28:05 +0100

    added e2e pipeline

---
+++ playwright.config.ts
@@ -0,0 +1,49 @@
+import type { PlaywrightTestConfig } from '@playwright/test'
+import { devices } from '@playwright/test'
+
+const config: PlaywrightTestConfig = {
+  expect: {
+    timeout: 5000,
+  },
+  forbidOnly: Boolean(process.env.CI),
+  fullyParallel: true,
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+
+    {
+      name: 'firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+      },
+    },
+
+    {
+      name: 'webkit',
+      use: {
+        ...devices['Desktop Safari'],
+      },
+    },
+  ],
+  reporter: process.env.CI ? 'html' : 'line',
+  retries: process.env.CI ? 2 : 0,
+  testDir: './tests',
+  timeout: 30 * 1000,
+  use: {
+    actionTimeout: 0,
+    trace: 'on-first-retry',
+  },
+  webServer: [
+    {
+      command: 'yarn serve',
+      port: 3000,
+    },
+  ],
+  workers: process.env.CI ? 1 : undefined,
+}
+
+export default config
