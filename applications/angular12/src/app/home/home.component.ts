2ffbd106667805fc40c324e12ffa8c8c989a163a
Umberto Toniolo <amountainram@gmail.com>
2023-05-09 11:43:34 +0200

    update playground and angular app

--- applications/angular12/src/app/home/home.component.ts
+++ applications/angular12/src/app/home/home.component.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core'
 
-import { assetUrl } from '../../micro-lc/asset-url'
-import type { AssetUrlPipe } from '../assetUrl.pipe'
+import { AssetUrlPipe } from '../assetUrl.pipe'
 
 @Component({
   styleUrls: ['./home.component.css'],
   templateUrl: './home.component.html',
 })
-export class HomeComponent implements AssetUrlPipe {
-  transform(value: string) {
-    return assetUrl(value)
-  }
-}
+export class HomeComponent extends AssetUrlPipe {}
